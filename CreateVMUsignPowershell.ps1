8251bf10d5d8262fa26fde6db4a7c3e5f94ebac4
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2022-02-07 17:46:13 +0530

    Added a new powershell commands for creating a new webapp

--- CreateVMUsignPowershell.ps1
+++ CreateVMUsignPowershell.ps1
@@ -40,6 +40,14 @@
 6. > Start-AzVm -Name "azVMName" -ResourceGroupName "powershellgroup"
 7. > Re]Start-AzVm -Name "azVMName" -ResourceGroupName "powershellgroup"
 
+# Command to create for Azure Webapp
+# 1. Create a new azure resource group live we have above
+# 2. Create a new app service plan
+     > New-AzAppServicePlan -ResourceGroupName "<Resource_Group_Name_Replace>" -Name "<App service plan name>" -Location "EastUs" -Tier "Free"
+     #Note: -Tier is based on usage
+# 3. Now its time to create a new webapp
+     > New-AzWebApp -ResourceGroupName "<Resource_Group_Name_Replace>" -Name "Name for your webapp" -Location "EastUs" -AppServicePlan "Add the app service plan"
+
 <#
   Even we can start and restart the VMs as well using this powershell
 #>
